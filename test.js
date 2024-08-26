20589305458216b196f928eb47d090a5c10a010b
PrincekJose <princejose262004@gmail.com>
2024-08-26 14:33:53 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
 const ggbtn = document.getElementById("ggbtn")
 ggbtn.addEventListener("click" ,() =>{
-
-    window.alert("This is a Simple Number guessing game...
-                 You Have 12 attempts:)   
-                 Have fun😊 ")
+window.alert("This is a Simple Number guessing game..You Have 12 attempts!!..Have fun😊 ") 
 })
